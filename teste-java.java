cd1de4779fb570d07033037df7eba2a96e1d49cd
13502P <114417695+13502P@users.noreply.github.com>
2022-09-26 15:23:23 +0100

    Add files via upload

---
+++ teste-java.java
@@ -0,0 +1,20 @@
+import java.util.*;  
+
+class hello {
+    public static void main(String[] args) {
+        int contagem = 1;
+        while (contagem < 2){
+
+        
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("insira o numero 1");
+        String str = sc.next();
+        System.out.println("insira o numero 2");
+        String str2=sc.next();
+        System.out.println("os valores inserido foi: "+str +str2);
+        sc.close();
+    }
+    contagem++;
+    }
+}
